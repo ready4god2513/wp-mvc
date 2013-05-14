043216d07271e6d135e10c07274632ade566c5fa
Brandon Hansen <brandonh@ibethel.org>
2013-05-13 21:30:34 -0700

    Merge with @apotropaic

--- core/pluggable/views/layouts/public.php
+++ core/pluggable/views/layouts/public.php
@@ -1,5 +1,5 @@
-<?php get_header(); ?>
+<?php
 
-<?php $this->render_main_view(); ?>
-
-<?php get_footer(); ?>
+get_header();
+$this->render_main_view();
+get_footer();
--- core/renders/mvc_variable_render.php
+++ core/renders/mvc_variable_render.php
@@ -14,5 +14,3 @@ class MvcVariableRender extends MvcRender{
 	}
 
 }
-
-?>
