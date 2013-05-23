936dbe2eea43267ea6353a6c319f8ddbe99499f2
Brandon Hansen <brandonh@ibethel.org>
2013-05-23 10:29:15 -0700

    Call in to the controller on method missing

--- core/renders/mvc_render.php
+++ core/renders/mvc_render.php
@@ -22,4 +22,25 @@ class MvcRender {
     $this->controller = $controller;
   }
 
+
+  /**
+   * Attempt to check to see if the called method is a method on the controller.
+   * The way that wpmvc calls into the renderer means that the controller is no longer
+   * the main object.
+   *
+   * @return void
+   * @author 
+   **/
+  public function __call($method, $args)
+  {
+    try
+    {
+      call_user_func_array(array($this->controller, $method), $args);
+    }
+    catch(Exception $e)
+    {
+      parent::__call($method, $args);
+    }
+  }
+
 }
